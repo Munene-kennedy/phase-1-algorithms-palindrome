6e2a48bdf4a65f86bbc3911a002bb9be8cedef2d
Kennedy-munene <kennedymunene309@gmail.com>
2022-12-08 23:48:29 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
+
+
+function reverseString(word) {
+
+  const wordArray = word.split("");
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join("");
+  return reversedWord;
+}
 function isPalindrome(word) {
   // Write your algorithm here
 }
+    const reversedWord = reverseString(word);
+    if (word === reversedWord) {
+      return true;
+    } else {
+      return false;
+    }
+  
 
 /* 
   Add your pseudocode here
 */
+  // type input string
+  // make a reverse of the input string
+  // if the input string seems the same as the reverse one
+  //   return true
+  // else
+  //   return false
 
 /*
   Add written explanation of your solution here
+  // create an array from the input string
+  // reverse the array
+  // create a string from the reversed array
+  // return the reversed string
 */
 
 // You can run `node index.js` to view these console logs
@@ -23,3 +49,5 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+module.exports = isPalindrome;
+
